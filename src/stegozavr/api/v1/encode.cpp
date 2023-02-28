ac925e0c3f3ec6c328f2c63ed23d1127e1b2e71e
ortur <orturb@yandex.ru>
2023-02-28 18:38:35 +0300

    just check

--- src/stegozavr/api/v1/encode.cpp
+++ src/stegozavr/api/v1/encode.cpp
@@ -1,12 +1,15 @@
 #include <stegozavr/api/v1/encode.hpp>
 
+
 namespace api::v1 {
 
 userver::formats::json::Value EncodeHandler::HandleRequestJsonThrow(const userver::server::http::HttpRequest& request,
                                                              const userver::formats::json::Value& request_json,
                                                              userver::server::request::RequestContext& context) const
 {
-  return userver::formats::json::Value();
+  namespace json = userver::formats::json;
+
+  return json::Value();
 }
 
 } // namespace api::v1
