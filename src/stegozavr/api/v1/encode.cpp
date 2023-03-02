6744bb8771e5fa4c4d881f372594b253deeb016e
ortur <orturb@yandex.ru>
2023-03-02 23:07:36 +0300

    Implemented reciving image in Base64

--- src/stegozavr/api/v1/encode.cpp
+++ src/stegozavr/api/v1/encode.cpp
@@ -1,5 +1,6 @@
 #include <stegozavr/api/v1/encode.hpp>
-
+#include <stegozavr/engine/encode/encode.hpp>
+#include <userver/crypto/base64.hpp>
 
 namespace api::v1 {
 
@@ -8,8 +9,17 @@ userver::formats::json::Value EncodeHandler::HandleRequestJsonThrow(const userve
                                                              userver::server::request::RequestContext& context) const
 {
   namespace json = userver::formats::json;
+  namespace base64 = userver::crypto::base64;
 
-  return json::Value();
+  auto file_content = base64::Base64Decode(request_json["file_content"].As<std::string>());
+  auto text_to_hide = request_json["text_to_hide"].As<std::string>();
+
+  // TODO: FIX BUG WITH ENCODING
+  // ::stegozavr::engine::blocking::Encode(file_content, {text_to_hide.begin(), text_to_hide.end()});
+
+  json::ValueBuilder value_builder;
+  value_builder["result"] = base64::Base64Encode(file_content);
+  return value_builder.ExtractValue();
 }
 
 } // namespace api::v1
