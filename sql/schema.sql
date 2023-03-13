a2ace7b8cbe9f3070997a28e12fe1883943c0fb1
ortur <orturb@yandex.ru>
2023-03-13 12:07:10 +0300

    Modified db script

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,17 +1,17 @@
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
     user_id serial PRIMARY KEY,
     email   varchar(40) NOT NULL
 );
 
 
-CREATE TABLE tokens
+CREATE TABLE IF NOT EXISTS tokens
 (
     token_id  serial PRIMARY KEY,
     token_val TEXT NOT NULL
 );
 
-CREATE TABLE users_to_token
+CREATE TABLE IF NOT EXISTS users_to_token
 (
     user_id  bigint REFERENCES users (user_id) ON DELETE CASCADE,
     token_id bigint REFERENCES tokens (token_id) ON DELETE CASCADE
