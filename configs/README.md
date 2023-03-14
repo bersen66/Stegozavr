b54b672bd349924eec855c29b132e2fa3f1b2041
ortur <orturb@yandex.ru>
2023-03-14 23:47:41 +0300

    Added PostgreSQL support to config files.

---
+++ configs/README.md
@@ -0,0 +1,5 @@
+# MOST IMPORTANT FOLDER.
+
+This directory contains config files that will be used when service starting.
+Config files will be copied to ```release``` folder. That is why you should modify them here.
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
-#include <stegozavr/api/handlers.hpp>
-#include <userver/utils/daemon_run.hpp>
+#include <userver/clients/dns/component.hpp>
 #include <userver/components/minimal_server_component_list.hpp>
+#include <userver/testsuite/testsuite_support.hpp>
+#include <userver/utils/daemon_run.hpp>
 
+#include <stegozavr/api/handlers.hpp>
 
 int main(int argc, char** argv)
 {
+  LOG_INFO() << "STEGOZAVR STARTED";
   // clang-format off
   const auto server_components =
       userver::components::MinimalServerComponentList()
-        .Append<api::v1::handlers::RegisterUser>()
+        .Append<userver::components::Postgres>("stegozavr-db")
+        .Append<userver::clients::dns::Component>()
+        .Append<userver::components::TestsuiteSupport>()
         .Append<api::v1::handlers::Decode>()
-        .Append<api::v1::handlers::Encode>();
+        .Append<api::v1::handlers::Encode>()
+        .Append<api::v1::handlers::RegisterUser>()
+        .Append<api::v1::handlers::GetToken>()
+      ;
   // clang-format on
+  LOG_INFO() << "COMPONENTS COMBINED";
   return userver::utils::DaemonMain(argc, argv, server_components);
 }
