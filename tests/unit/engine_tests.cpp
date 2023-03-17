511dc7cbd9153a6cb8f1284d7694e7669d6b96a3
ortur <orturb@yandex.ru>
2023-03-17 19:48:58 +0300

    Added more unit-tests

--- tests/unit/engine_tests.cpp
+++ tests/unit/engine_tests.cpp
@@ -1,13 +1,67 @@
 #include <iostream>
 #include <gtest/gtest.h>
 #include <stegozavr/engine/engine.hpp>
+#include <fstream>
+
+#include <in_image_hider/hider_png.h>
 
 /**
  * Just checking that it works
  */
-TEST(Encode, 1) {
 
-  ASSERT_EQ(1, 1);
+namespace engine = ::stegozavr::engine::blocking;
+
+
+
+TEST(TestEncode, 1) {
+  std::fstream test_src("resources/test1_src.png", std::ios::binary | std::ios::in | std::ios::out);
+
+  std::stringstream output_image(std::ios::binary | std::ios::in | std::ios::out);
+
+  hider_png::custom::encode(test_src, "Hello, world", output_image);
+  ASSERT_EQ(::hider_png::custom::decode(output_image), "Hello, world");
+}
+
+
+TEST(TestEncode, 2) {
+  std::fstream test_src("resources/test1_src.png", std::ios::binary | std::ios::in | std::ios::out);
+  std::stringstream output_image(std::ios::binary | std::ios::in | std::ios::out);
+
+  hider_png::custom::encode(test_src, "", output_image);
+
+  ASSERT_EQ(::hider_png::custom::decode(output_image), "");
+}
+
+TEST(TestEncode, 3) {
+  std::fstream test_src("resources/test1_src.png", std::ios::binary | std::ios::in | std::ios::out);
+  std::stringstream output_image(std::ios::binary | std::ios::in | std::ios::out);
+
+  hider_png::custom::encode(test_src, "abOOOOOOOOOOOOOOOOOOOOOOOOBA", output_image);
+
+  ASSERT_EQ(::hider_png::custom::decode(output_image), "abOOOOOOOOOOOOOOOOOOOOOOOOBA");
+}
+
+TEST(TestEncode, 4) {
+  std::fstream test_src("resources/test1_src.png", std::ios::binary | std::ios::in | std::ios::out);
+  std::stringstream output_image(std::ios::binary | std::ios::in | std::ios::out);
+
+  hider_png::custom::encode(test_src, "Шлепа - большой русский кот", output_image);
+
+  ASSERT_EQ(::hider_png::custom::decode(output_image), "Шлепа - большой русский кот");
+}
+
+TEST(TestEncode, 5) {
+  std::fstream test_src("resources/test1_src.png", std::ios::binary | std::ios::in | std::ios::out);
+  std::stringstream output_image(std::ios::binary | std::ios::in | std::ios::out);
+
+  hider_png::custom::encode(test_src, "Шлепа - большой русский кот", output_image);
+
+
+  ASSERT_EQ(::hider_png::custom::decode(output_image), "Шлепа - большой русский кот");
+
+
+  std::ofstream result("output.png", std::ios::binary);
+  result << output_image.str();
 }
 
 int main() {
